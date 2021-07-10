8555597cf1d51af5d0e7045ba2599f84589696ae
Jens <jfischer77@gmail.com>
2021-07-10 22:31:14 +0200

    used recursive approach

--- 01/aoc_01.py
+++ 01/aoc_01.py
@@ -19,30 +19,32 @@ def mult_all(list):
 	return r
 
 
-
-def find_pair(input_list, tuple_size):
-	if tuple_size == 2:
-		for i1 in range(len(input_list)):
-			for i2 in range(i1+1, len(input_list)):
-				tuple = (input_list[i1], input_list[i2])
-				if add_all(tuple) == 2020:
-					return tuple
-	elif tuple_size == 3:
-		for i1 in range(len(input_list)):
-			for i2 in range(i1+1, len(input_list)):
-				for i3 in range(i2+1, len(input_list)):
-					tuple = (input_list[i1], input_list[i2], input_list[i3])
-					if add_all(tuple) == 2020:
-						return tuple
-	return (0,0)
+def sub_sum(max_tuple, input_list, index_list=[], current_sum=0):
+	if len(index_list) == max_tuple:
+		return (current_sum, index_list)
+	for i in range(len(input_list)):
+		if i in index_list:
+			continue
+		sub_result = sub_sum(max_tuple, input_list, index_list+[i], current_sum+input_list[i])
+		if sub_result == None:
+			continue
+		if sub_result[0] != 2020:
+			continue
+		return sub_result
+		
 
 
-def product_of_pair(input_file, tuple_size):
+def find_tuple(input_list, tuple_size):
+	result = sub_sum(tuple_size, input_list)
+	return list(map(lambda x: input_list[x], result[1]))
+		
+
+def product_of_tuple(input_file, tuple_size):
 	list = list_from_file(input_file)
-	pair = find_pair(list, tuple_size)
+	pair = find_tuple(list, tuple_size)
 	return mult_all(pair)
     
 
 if __name__ == "__main__":
-	print("result for {}:{}".format(2, product_of_pair("source.txt", 2)))
-	print("result for {}:{}".format(3, product_of_pair("source.txt", 3)))
+	print("result for {}:{}".format(2, product_of_tuple("source.txt", 2)))
+	print("result for {}:{}".format(3, product_of_tuple("source.txt", 3)))
